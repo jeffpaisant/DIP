1c9c83a8f3c9c884893543ea27eac5e9910b7edf
jfpaisant <jean-francois.paisant@outlook.com>
2020-03-07 13:47:06 +0100

    Add test for email sender

---
+++ csharp/Tests/EmailSenderShould.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using FluentAssertions;
+using Soat.CleanCoders.DipKata.Main;
+using Xunit;
+
+namespace Soat.CleanCoders.DipKata.Tests
+{
+    public class EmailSenderShould
+    {
+        private TextWriter _consoleOut;
+
+        private string MailBuffer => _consoleOut.ToString();
+        public EmailSenderShould()
+        {
+            _consoleOut = new StringWriter();
+            Console.SetOut(_consoleOut);
+        }
+
+        [Fact]
+        public void SendMessage_When_Called()
+        {
+            var sender = new EmailSender();
+            var mail = "myEmail";
+            var message = "myWonderfulMessage";
+            var expected = $"To:{mail}, Subject: Happy birthday!, Message: {message}";
+
+            sender.Send(mail, message);
+
+            MailBuffer.Should().Be(expected);
+        }
+    }
+}
