ed9fa03639522f1099fd730c4f29db319365fac0
jfpaisant <jean-francois.paisant@outlook.com>
2020-03-07 17:02:18 +0100

    implement FriendRepository

---
+++ csharp/Repository/FriendRepository.cs
@@ -0,0 +1,48 @@
+﻿using Soat.CleanCoders.DipKata.Main;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Soat.CleanCoders.DipKata.Repository
+{
+    public class FriendRepository : IFriendRepository
+    {
+        private ILoader _loader;
+        private IEnumerable<Friend> _friends;
+
+        public FriendRepository(ILoader loader)
+        {
+            _loader = loader;
+        }
+
+        public IEnumerable<Friend> FindFriendsBornOn(DateTime dayOfBirth)
+        {
+            _friends = GetFriends();
+            return _friends.Where(f => f.DateOfBirth == dayOfBirth);
+        }
+
+        private IEnumerable<Friend> GetFriends()
+        {
+            _loader.Load();
+            var storedFriends = _loader.GetFriends();
+            var friends = new List<Friend>();
+            foreach (var sf in storedFriends)
+            {
+                var dob = DateTime.Parse(sf.date_of_birth);
+                var friend = MapStoredFriendToFriend(sf, dob);
+                friends.Add(friend);
+            }
+            return friends;
+        }
+
+        private static Friend MapStoredFriendToFriend(StoredFriend sf, DateTime dob)
+        {
+            var builder = new FriendBuilder();
+            var friend = builder.WithFirstName(sf.first_name)
+                .WithLastName(sf.last_name)
+                .WithEmail(sf.email)
+                .WithDateOfBirth(dob).Build();
+            return friend;
+        }
+    }
+}
--- csharp/Tests/FriendRepositoryShould.cs
+++ csharp/Tests/FriendRepositoryShould.cs
@@ -1,16 +1,26 @@
-﻿using System;
-using System.IO;
-using FluentAssertions;
-using Soat.CleanCoders.DipKata.FriendRepository;
-using Soat.CleanCoders.DipKata.Main;
-using Soat.CleanCoders.DipKata.Sender;
+﻿using FluentAssertions;
+using Moq;
+using Soat.CleanCoders.DipKata.Repository;
+using System;
+using System.Linq;
 using Xunit;
 
 namespace Soat.CleanCoders.DipKata.Tests
 {
     public class FriendRepositoryShould
     {
+        [Fact]
+        public void Return_An_Empty_List_When_No_Friends()
+        {
+            var loader = new Mock<ILoader>();
+            loader.Setup(r => r.GetFriends()).Returns(Enumerable.Empty<StoredFriend>());
 
-      
+            var repository = new FriendRepository(loader.Object);
+
+            var friends = repository.FindFriendsBornOn(DateTime.Now);
+
+            friends.Should().BeEmpty();
+        }
+
     }
 }
