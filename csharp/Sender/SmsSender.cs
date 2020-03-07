d9d0386524649f51551d58ef8fe9b18c0ff0481f
jfpaisant <jean-francois.paisant@outlook.com>
2020-03-07 14:45:13 +0100

    Make SmsSender inherits from Sender

--- csharp/Sender/SmsSender.cs
+++ csharp/Sender/SmsSender.cs
@@ -1,12 +1,18 @@
-﻿using System;
+﻿using Soat.CleanCoders.DipKata.Main;
+using System;
 
 namespace Soat.CleanCoders.DipKata.Sender
 {
-    public class SmsSender : ISender
+    public class SmsSender : Sender, ISender
     {
         public void Send(string friendName)
         {
             Console.Write($"Happy birthday, my dear {friendName}!");
+        }
+
+        public override void Send(Friend friend, string message)
+        {
+            Send(friend.FirstName);
         }
     }
 }
