181aa3b5dab2b63e3027667955c2ebc8e69d116c
jfpaisant <jean-francois.paisant@outlook.com>
2020-03-07 16:32:13 +0100

    Create implementation of IFriendRepository

---
+++ csharp/FriendRepository/FriendRepository.cs
@@ -0,0 +1,15 @@
+﻿using Soat.CleanCoders.DipKata.Main;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Soat.CleanCoders.DipKata.FriendRepository
+{
+    public class FriendRepository : IFriendRepository
+    {
+        public IEnumerable<Friend> FindFriendsBornOn(DateTime today)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
