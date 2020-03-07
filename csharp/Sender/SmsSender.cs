371dd3396822db584daa6a5f345e7e11340b6468
jfpaisant <jean-francois.paisant@outlook.com>
2020-03-07 14:01:14 +0100

    Create SmsSender

---
+++ csharp/Sender/SmsSender.cs
@@ -0,0 +1,13 @@
+﻿using Soat.CleanCoders.DipKata.Main;
+using System;
+
+namespace Soat.CleanCoders.DipKata.Sender
+{
+    public class SmsSender : ISender
+    {
+        public void Send(string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
